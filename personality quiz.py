a764f94bc2cf150b0fdbff888b6629306e52c35f
mgree205 <156396250+mgree205@users.noreply.github.com>
2024-04-22 21:35:52 -0400

    change #3
    Made small changes to my personality results from the question asked beforehand.

--- personality quiz.py
+++ personality quiz.py
@@ -4,17 +4,18 @@ print('Answer with (yes, no, sometimes)')
 per = 0
 persona = ['Why do you keep pushing the no word ', 'You are a rebel who likes to try new things',
            'You are an adventurer who don\'t fear death', 'You have a courageous character', 'You don\'t follow the herd',
-           'You like living but want to change your life', 'You are brave but don\'t like adventure',
-           'You don\'t care about what people say about you', 'You never stop helping your friends',
-           'You care about what people say too much', 'You have a huge potential',
-           'You fear darkness', 'Cheater alarm!! You keep pressing the yes button 😂']
+           'You like living but want to change your life', 'You are great at loyal to everyone but yourself',
+           'You don\'t care about what people say about you', 'You never stop helping your friends, which is your problem',
+           'You care about what people say too much', 'You have potiental to suceed',
+           'You do not fear darkness']
 
 
 def ask():
     global per
     per = 0
     Q = ('Do you easliy get mad?', 'Would you say your a out-going person?', 'Do you thrive in high-pressure situations?', 'DO you wake up early?',
-         'Are you good at keeping secrets?', 'Do you like following rules 😉','Are you most likely to be loyal to the ones you love?', 'Are you most likely to standup for your beliefs?')
+         'Are you good at keeping secrets?', 'Do you like following rules ','Are you most likely to be loyal to the ones you love?', 'Are you most likely to standup for your beliefs?',
+         'I take care of other people before taking care of myself','I am life of the party','I am skilled on handling awkward situations')
     for i in Q:
         UInp = (input(i))
         if UInp == 'yes':
