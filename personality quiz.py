49391599fd543a03ee452e6c455f6357b69e21ba
mgree205 <156396250+mgree205@users.noreply.github.com>
2024-04-22 20:44:14 -0400

    Change #1 to code
    I change personality questions to  what I want it to ask players

--- personality quiz.py
+++ personality quiz.py
@@ -1,1 +1,34 @@
-#comment
+print('Hi player!!')
+print('Let\'s do a quiz to know your personality!!')
+print('Answer with (yes, no, sometimes)')
+per = 0
+persona = ['Why do you keep pushing the no word 😂', 'You are a rebel who likes to try new things',
+           'You are an adventurer who don\'t fear death', 'You have a courageous character', 'You don\'t follow the herd',
+           'You like living but want to change your life', 'You are brave but don\'t like adventure',
+           'You don\'t care about what people say about you', 'You never stop helping your friends',
+           'You care about what people say too much', 'You have a huge potential',
+           'You fear darkness', 'Cheater alarm!! You keep pressing the yes button 😂']
+
+
+def ask():
+    global per
+    per = 0
+    Q = ('Do you easliy get mad?', 'Would you say our out-going person?', 'Do you thrive in high-pressure situations?', 'DO you wake up early?',
+         'Do you go swimming?', 'Do you have a crush 😉',)
+    for i in Q:
+        UInp = (input(i))
+        if UInp == 'yes':
+            per += 2
+        elif UInp == 'no':
+            per += 0
+        else:
+            per += 1
+    print(persona[per])
+    UInp = input('Do you want to play again?')
+    if UInp == 'yes':
+        ask()
+    else:
+        quit()
+
+
+ask()
